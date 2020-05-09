be565ce4af43223aaf0ce1e4fe1c06caa9d9b2bb
Beeeam <59039964+Beeeam@users.noreply.github.com>
2020-05-09 14:24:18 +0800

    Update SVM.py

--- ch06-SVM/SVM.py
+++ ch06-SVM/SVM.py
@@ -5,7 +5,7 @@ def loaddata(filename):
 	m = len(fr)
 	for line in fr:
 		linearr = line.stripe().split('\t')
-		datamat.append(float(linearr[:m-1]))
-		labelmat.append(float(linearr[-1]))
-	return datamat,labelmat
+		datamat.append(linearr[:m-1])
+		labelmat.append(linearr[-1])
+	return np.array(datamat,dtype=np.float64),np.(labelmat,dtype=np.int)
 		
